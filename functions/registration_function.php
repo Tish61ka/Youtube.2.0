4fe29b7cfed4ef13f6429c159cd00083bf756b08
Daniil <danchik.kun@mail.ru>
2022-02-19 15:59:57 +0300

    Добавил проверку на одинаковую почту

--- functions/registration_function.php
+++ functions/registration_function.php
@@ -12,7 +12,7 @@
     if ($password === $password_confirm){
         $avatar = 'img/' . time() . $_FILES['avatar']['name'];
         move_uploaded_file($_FILES['avatar']['tmp_name'], '../' . $avatar);
-        // header('Location: ../output/singin.php'); 
+        header('Location: ../output/singin.php'); 
         // $password = md5($password);
         $check_nick_name = $connect->prepare("SELECT * FROM `users` WHERE `nick_name` = '$nick_name'");
         $check_nick_name->execute();
@@ -22,10 +22,19 @@
             header('Location: ../output/registration.php'); 
         } 
         else{
-            $add_user = $connect->prepare("INSERT INTO `users`(`id`, `nick_name`, `email`, `avatar`, `password`) VALUES (NULL,'$nick_name','$email','$avatar','$password')");
-            $add_user->execute();
-            $_SESSION['message'] = "Регистрация прошла успешно!";
-            header('Location: ../output/singin.php');
+            $check_email = $connect->prepare("SELECT * FROM `users` WHERE `email` = '$email'");
+            $check_email->execute();
+            $response1 = $check_email->fetch(PDO::FETCH_ASSOC);
+            if($response1['email']){
+                $_SESSION['message'] = "Такая почта уже используется!!!";
+                header('Location: ../output/registration.php'); 
+            }
+            else{
+                $add_user = $connect->prepare("INSERT INTO `users`(`id`, `nick_name`, `email`, `avatar`, `password`) VALUES (NULL,'$nick_name','$email','$avatar','$password')");
+                $add_user->execute();
+                $_SESSION['message'] = "Регистрация прошла успешно!";
+                header('Location: ../output/singin.php');
+            }            
         }     
     }
     else{
