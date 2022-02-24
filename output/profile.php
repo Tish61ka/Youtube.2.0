d35dee06cbc41f56acc45e5c59c1af94119083c6
Daniil <danchik.kun@mail.ru>
2022-02-24 21:41:50 +0300

    Начал реализовывать функцию загрузки видео

--- output/profile.php
+++ output/profile.php
@@ -22,7 +22,24 @@ if (!$_SESSION['user']){
 </header>
 <body>
         <section>
+            <form action="../functions/load_video.php" method="post" enctype="multipart/form-data">
+                <label for="">Загрузите видео</label> <br>
+                <input type="file" name="video" id=""> <br>
 
+                <label for="">Введите название видео</label> <br>
+                <input type="text" name="name_video" placeholder="Название видео"> <br>
+
+                <label for="">Описание</label> <br>
+                <textarea name="discription" id="" cols="30" rows="10" placeholder="Введите описание видео"></textarea> <br>
+
+                <input type="submit" value="Загрузить">
+                <?php 
+                        if ($_SESSION['message']){
+                            echo '<p class="message">' . $_SESSION['message'] . '</p>';
+                        }
+                        unset($_SESSION['message']);
+                ?> 
+            </form>
         </section>
         <a href="../functions/logout.php">Выход</a>
 </body>
