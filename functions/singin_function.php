ece7b8b489293e8279c1e7f39ea92ef24e2a48ff
Tishka <yuriitish@mail.ru>
2022-03-01 20:10:54 +0400

    Администрация

--- functions/singin_function.php
+++ functions/singin_function.php
@@ -8,20 +8,31 @@
     $password = $_POST['password'];
 
     $check_user = $connect->prepare("SELECT * FROM `users` WHERE `nick_name` = '$nick_name' AND `password` = '$password'");
-    $check_user->execute();
-    
-     $user = $check_user->fetch(PDO::FETCH_ASSOC);
+    $check_user->execute();    
+    $user = $check_user->fetch(PDO::FETCH_ASSOC);
+    print_r($user);
     if ($user>0){
-        $_SESSION['user'] = [
-            "id" => $user['id'],
-            "nick_name" => $user['nick_name'],
-            "avatar" => $user['avatar'],
-            "email" => $user['email']
-        ];
-        
-        header('Location: ../output/profile.php');
+        if($user['role']==1){
+            $_SESSION['admin'] = [
+                "id" => $user['id'],
+                "nick_name" => $user['nick_name'],
+                "avatar" => $user['avatar'],
+                "email" => $user['email'],
+                "role" => $user['role']
+            ];
+            header('Location: ../output/adminprofile.php');
+        }
+        else{
+            $_SESSION['user'] = [
+                "id" => $user['id'],
+                "nick_name" => $user['nick_name'],
+                "avatar" => $user['avatar'],
+                "email" => $user['email']
+            ];
+            header('Location: ../output/profile.php');
+        }        
     }
     else{
         $_SESSION['message'] = 'Неверный логин или пароль!!!';
-        header('Location: ../output/singin.php');  
+        header('Location: ../output/singin.php');
     } 
---
+++ output/adminprofile.php
@@ -0,0 +1,72 @@
+<?php
+session_start();
+if (!$_SESSION['admin']){
+    header('Location: adminprofile.php');
+}
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/adminprofile.css">
+    <title>Профиль</title>
+</head>
+<header>
+    <a href=""><img src="../pictures/лого.png" class="logo" alt=""></a>
+</header>
+<body>
+<div class="line-right">
+<div class="icon">
+          <p>Никнейм:<?=$_SESSION['admin']['nick_name']?></p>
+          <p>Почта:<?=$_SESSION['admin']['email']?></p>
+    </div>
+    <ul class="ul_profile">
+        <a href="adminprofile.php">
+            <li class="between_li">
+                Profile
+            </li>
+        </a>
+        <a href="">
+            <li>
+                Download video
+            </li>
+        </a>
+        <a href="">
+            <li class="last_li">
+                My video
+            </li>
+        </a>
+    </ul>
+</div>
+        <section>
+            <div class="div_nickname">
+                <ul>
+                    
+                <?php
+                    require_once('../functions/connect.php');
+                    $result = $connect->prepare("SELECT * FROM `users`");
+                    $result->execute();
+                    while($row = $result->fetch(PDO::FETCH_ASSOC)){
+                        ?>
+                            <li><p>ID:<?=$row['id']?></p></li>
+                            <li><p>Никнейм:<?=$row['nick_name']?></p></li>
+                            <li><p>Почта:<?=$row['email']?></p></li>
+                            <li><p>Пароль:<?=$row['password']?></p></li>
+                            <li><p>Роль:<?=$row['role']?></p></li> 
+                            <a href="../functions/delete_user.php"><button>Удалить пользователя</button></a> <br>
+                        <?php
+                    }
+                ?>
+                </ul>
+            </div>
+        </section>
+        <a class="logout" href="../functions/logout.php">
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                width="54" height="54"
+                viewBox="0 0 172 172"
+                style=" fill:#000000;"><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,172v-172h172v172z" fill="none"></path><g fill="#ba45f0"><path d="M164.83333,21.66797l-57.33333,7.11068v21.38802h14.33333v-8.72038l28.66667,-3.56934v96.2461l-28.66667,-3.56934v-8.72038h-14.33333v21.38802l57.33333,7.11067zM73.45833,28.66667c-6.92657,0 -12.54167,5.6151 -12.54167,12.54167c0,6.92657 5.6151,12.54167 12.54167,12.54167c6.92657,0 12.54167,-5.6151 12.54167,-12.54167c0,-6.92657 -5.6151,-12.54167 -12.54167,-12.54167zM35.58138,50.16667c-2.8547,0.01075 -5.70892,0.59996 -8.38444,1.74967l-0.09798,0.028l-13.08757,6.04688l6.01888,13.01758l12.82161,-5.9349h0.02799c0.88579,-0.37659 1.82295,-0.57037 2.75749,-0.57389c0.95439,-0.00359 1.9023,0.18308 2.79948,0.5599l3.35938,1.51172l-11.68782,21.54199c-0.96033,1.97083 -1.45607,4.16215 -1.44173,6.34082c0.0215,3.79833 1.51172,7.36262 4.19922,10.05012l17.28678,14.09538l-8.39844,24.7334h15.13118l6.83073,-20.10026c0.59483,-1.70566 0.85552,-3.47885 0.78385,-5.24902c-0.1505,-3.81983 -1.77308,-7.35658 -4.95508,-10.30208l-8.5944,-8.46843l10.66602,-19.66634l2.49154,3.83529c3.96191,6.09854 10.76322,9.78418 18.02865,9.78418h11.02994v-14.33333h-11.02994c-2.43825,0 -4.69179,-1.21861 -6.01888,-3.26139l-5.10905,-7.85254v-0.014c-2.2502,-3.45034 -5.43828,-6.20108 -9.22428,-7.89453l-17.7207,-7.93652l-0.08398,-0.02799c-2.68615,-1.12818 -5.54016,-1.69043 -8.39844,-1.67969zM121.83333,64.5v14.33333h-14.33333v14.33333h14.33333v14.33333l21.5,-21.5zM27.07096,108.67578l-3.05143,3.62533c-1.3545,1.505 -3.28368,2.36556 -5.31901,2.36556h-11.53385v14.33333h11.53385c6.09167,0 11.911,-2.59131 16.125,-7.29264l3.19141,-3.7793l-10.2181,-8.35645c-0.27233,-0.27233 -0.46986,-0.60916 -0.72786,-0.89583z"></path></g></g></svg>    
+        </a>
+</body>
+</html>
